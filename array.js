7554f9fca11224bd3b3896c9f326b93039cda8d5
Daniel-Davis <ddv9461@saintjoe.edu>
2015-05-06 11:22:18 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,1 +1,10 @@
 //Daniel Davis, array, creates an array with two properties(name & value) and returns atleast two objects of the array will be returned //
+
+var part = {name:"", value: false} // creates an empty constructor for parts with a string name and a boolean value
+var arry = [var brains = new part{name:"brains", value: true},var eyes = new part{name:"eyes", value: false}, var legs = new part{name:"legs", value: false}, var organs = new part{name:"organs", value: true}];] // array of human parts that may be yummy to a zombie
+
+for (var i = 0, i<arry.length, ++i) { // looks at each part of the human to see if it's yummy
+  if([i.value] == true) { // if its yummy value is true
+    print([i]); // let me know by showing me
+  }// end of if statement
+}// end of for loop
